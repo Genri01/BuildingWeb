96683289d4224e7b89e7ea060b604f799a452d12
Slick <cnbv170291@yandex.ru>
2023-01-10 23:43:43 +0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,40 @@
+import React, { useEffect } from 'react';
+import Main from './routes/index';
+import { isMobile } from 'react-device-detect';
+import { useDispatch, useSelector } from 'react-redux';
+import { change_page, setMobileMod } from './redux/actions/app';
+import { pages, app } from './redux/selectors';
+import HeaderMenu from './components/HeaderMenu';
+import './App.css';
+
+function App() {
+  const dispatch = useDispatch();  
+
+  const page = useSelector(pages.page); 
+  const mobile = useSelector(app.mobile); 
+
+  useEffect(() => {
+    if(localStorage.getItem('page') === null) {  
+      localStorage.setItem('page','main'); 
+    }   
+    dispatch(change_page(localStorage.getItem('page')));
+    dispatch(setMobileMod(isMobile));
+  },[]);
+
+  
+  return (
+    <div className="App">
+      <HeaderMenu 
+        mobile={mobile}  
+        page={page} 
+        onClick={(e) => {  
+          localStorage.setItem('page',e.target.id);
+          dispatch(change_page(e.target.id));
+        }}   
+      />
+      {/* <Main mobile={mobile} /> */}
+    </div>
+  );
+}
+
+export default App;
